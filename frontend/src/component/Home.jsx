478d2dfcfe7269bfa8be1b2d0da23e675deeacdb
Atulya49 <atulyagupta49@gmail.com>
2024-12-19 23:43:25 +0530

    Dover logo changed

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Background from "../assets/Monitor.png";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 import { Link } from "react-router-dom";
-
+import doverimage from "../assets/doverimage.png";
 function Home() {
   return (
     <div className="relative h-screen">
@@ -12,13 +17,9 @@ function Home() {
             href="https://flowbite.com"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Dover Logo"
-            />
+            <img src={doverimage} className="h-8" alt="Dover Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Dover
+              Dover Engineering COE
             </span>
           </a>
         </div>
@@ -37,13 +38,13 @@ function Home() {
                       <Link to="/opdprod">Production</Link>
                     </DropdownMenuItem>
                     <DropdownMenuItem className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-gray-100">
-                      <Link to='/opdtest'>Testing</Link>
+                      <Link to="/opdtest">Testing</Link>
                     </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
               </li>
               <li>
-              <DropdownMenu>
+                <DropdownMenu>
                   <DropdownMenuTrigger className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-blue-600 hover:bg-gray-100 h-10 px-4 py-2">
                     CPC
                   </DropdownMenuTrigger>
@@ -52,13 +53,13 @@ function Home() {
                       <Link to="/cpcprod">Production</Link>
                     </DropdownMenuItem>
                     <DropdownMenuItem className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-gray-100">
-                      <Link to='/cpctest'>Testing</Link>
+                      <Link to="/cpctest">Testing</Link>
                     </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
               </li>
               <li>
-              <DropdownMenu>
+                <DropdownMenu>
                   <DropdownMenuTrigger className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-blue-600 hover:bg-gray-100 h-10 px-4 py-2">
                     MAAG
                   </DropdownMenuTrigger>
@@ -67,7 +68,7 @@ function Home() {
                       <Link to="/maagprod">UAT</Link>
                     </DropdownMenuItem>
                     <DropdownMenuItem className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-gray-100">
-                      <Link to='/maagtest'>Dev</Link>
+                      <Link to="/maagtest">Dev</Link>
                     </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
